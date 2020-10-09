d7044f47f6abe470826d4b74d0175a8cf351649d
khml <43922054+khml@users.noreply.github.com>
2020-10-09 23:45:19 +0900

    [Add] test code for tokenize

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-#[derive(PartialOrd, PartialEq, Debug)]
+#[derive(PartialOrd, PartialEq, Eq, Debug)]
 pub enum TokenType {
     Equal,
     Number,
@@ -12,7 +12,7 @@ pub enum TokenType {
     Whitespace,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Token {
     pub t_type: TokenType,
     pub value: String,
@@ -45,8 +45,10 @@ pub fn tokenize(sentence: &str) -> Result<Vec<Token>, String> {
 
 #[cfg(test)]
 mod tests {
+    use super::Token;
     use super::TokenType;
     use super::get_token_type;
+    use super::tokenize;
 
     #[test]
     fn test_get_token_type() {
@@ -59,5 +61,35 @@ mod tests {
         assert_eq!(get_token_type(')').unwrap(), TokenType::ParenthesisRight);
         assert_eq!(get_token_type('%').unwrap(), TokenType::Percent);
         assert_eq!(get_token_type(' ').unwrap(), TokenType::Whitespace);
+    }
+
+    #[test]
+    fn test_tokenize() {
+        let expected = vec![];
+        assert_eq!(tokenize("").unwrap(), expected);
+        assert_eq!(tokenize(" ").unwrap(), expected);
+        assert_eq!(tokenize("   ").unwrap(), expected);
+
+        let expected = vec![Token { t_type: TokenType::Plus, value: "+".to_string() }];
+        assert_eq!(tokenize("+").unwrap(), expected);
+
+        let expected = vec![
+            Token { t_type: TokenType::Number, value: "1".to_string() },
+            Token { t_type: TokenType::Plus, value: "+".to_string() },
+            Token { t_type: TokenType::Number, value: "2".to_string() },
+        ];
+        assert_eq!(tokenize("1 + 2").unwrap(), expected);
+        assert_eq!(tokenize("1+2").unwrap(), expected);
+
+        let expected = vec![
+            Token { t_type: TokenType::ParenthesisLeft, value: "(".to_string() },
+            Token { t_type: TokenType::Number, value: "1".to_string() },
+            Token { t_type: TokenType::Plus, value: "+".to_string() },
+            Token { t_type: TokenType::Number, value: "2".to_string() },
+            Token { t_type: TokenType::ParenthesisRight, value: ")".to_string() },
+            Token { t_type: TokenType::Slash, value: "/".to_string() },
+            Token { t_type: TokenType::Number, value: "3".to_string() },
+        ];
+        assert_eq!(tokenize("(1 + 2) / 3").unwrap(), expected);
     }
 }
